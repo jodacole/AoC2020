cd9a5fb84edc575d451657628a9b66be8e31753c
Josh Cole <jdc122897@yahoo.com>
2021-12-06 00:09:00 -0500

    initial commit

---
+++ day1/day1.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log(part1(data));
+  }
+});
+
+function part1(data) {
+  numbers = data.split("\n").map((n) => parseInt(n, 10));
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === 2020) {
+        return numbers[i] * numbers[j];
+      }
+    }
+  }
+}
