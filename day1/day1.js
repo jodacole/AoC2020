38d930f71b163ba8ff3ca89f0af2bbcfcf69ef60
Josh Cole <jdc122897@yahoo.com>
2021-12-06 00:12:01 -0500

    day1 finished

--- day1/day1.js
+++ day1/day1.js
@@ -6,6 +6,7 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     return;
   } else {
     console.log(part1(data));
+    console.log(part2(data));
   }
 });
 
@@ -15,6 +16,19 @@ function part1(data) {
     for (let j = i + 1; j < numbers.length; j++) {
       if (numbers[i] + numbers[j] === 2020) {
         return numbers[i] * numbers[j];
+      }
+    }
+  }
+}
+
+function part2(data) {
+  numbers = data.split("\n").map((n) => parseInt(n, 10));
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      for (let k = j + 1; k < numbers.length; k++) {
+        if (numbers[i] + numbers[j] + numbers[k] === 2020) {
+          return numbers[i] * numbers[j] * numbers[k];
+        }
       }
     }
   }
